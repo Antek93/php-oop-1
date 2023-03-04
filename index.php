09df1862a0b4e468c64b323b5fe78bf823db91c3
Antek93 <antonio.savino1993@gmail.com>
2023-03-04 12:47:54 +0100

    rimossi file di prova - esercizio completato

--- index.php
+++ index.php
@@ -2,18 +2,18 @@
     // 1.) Definire la classe Movie                                               
     class Movie { 
     // 2.) Definire all'interno della classe le variabili d'istanza:       
-        public $title;
-        public $genre;
+        public $title = 'Titolo del film'; //Impostato valore di DEFAULT, nel caso in cui il valore non venisse specificato succ..
+        private $genre;
         private $length;
         private $rating;
 
-    // 3.) Deve essere presente almeno una funzione:
+    // 3.) Deve essere presente almeno un metodo:
 
     public function getLength($length) {
         return $this->length = $length;
     }
 
-    public function setRating($rating) {
+    public function setRating($rating = 0) { //Se non viene passato nessun valore, il valore di default è 0
 
         if (is_numeric($rating)) { // Se il valore è passato è di tipo numerico
             $this->rating = $rating; // rating passa attraverso la funzione pubblica e diventa visibile
@@ -24,6 +24,15 @@
         
     }
 
+    // 4.) All'interno della classe è presente almento un costruttore
+
+    // ### Se si definisce un costruttore con UN argomento, quando si crea un'istanza si DEVE passare UN argomento
+    public function __construct($_genre) {
+
+        $this->genre = $_genre;
+
+    }
+
 
     }
 
@@ -31,9 +40,9 @@
     
     // ------| ISTANZA MATRIX | ------ della classe Movie
 
-    $matrix = new Movie ();
-    $matrix->title = 'Matrix'; //le istanze sono dichiarate senza DOT NOTATION
-    $matrix->genre = 'Action'; //in php viene utilizzata la freccia 
+    $matrix = new Movie ('Action');     // ### Se si definisce un costruttore con UN argomento, quando si crea un'istanza si DEVE passare UN argomento
+
+    $matrix->title = 'Matrix'; //le istanze sono dichiarate senza DOT NOTATION //in php viene utilizzata la freccia 
     $matrix->getLength('120m');
     $matrix->setRating(4.5);
 
@@ -43,9 +52,9 @@
 
     // ------| ISTANZA RUSH | ------ della classe Movie
 
-    $rush = new Movie ();
-    $rush->title = 'Rush'; //le istanza sono dichiarate senza DOT NOTATION
-    $rush->genre = 'Sport'; //in php viene utilizzata la freccia 
+    $rush = new Movie ('Sport');     // ### Se si definisce un costruttore con UN argomento, quando si crea un'istanza si DEVE passare UN argomento
+
+    $rush->title = 'Rush'; //le istanza sono dichiarate senza DOT NOTATION //in php viene utilizzata la freccia 
     $rush->getLength('90m');
     $rush->setRating(3.5);
 
