2afc294e829f5ec978ec2ca332ac21594b8cd91c
Antek93 <antonio.savino1993@gmail.com>
2023-03-04 11:15:04 +0100

    creata classe e definite le variabili d'istanza

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+    // 1.) Definire la classe Movie 
+    class Movie { 
+    // 2.) Definire all'interno della classse le variabili d'istanza:
+        public $title;
+        public $genre;
+        public $length;
+        public $rating;
+    }
+
+    
+
+?>
